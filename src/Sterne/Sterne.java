d1f95ba7d154edb19beb17c519d1a304e5bad67b
kaufmann.aslan_alexa <kaufmann.aslan_alexa@PC230110.osz.intern>
2016-12-21 13:14:30 +0100

    kock

---
+++ src/Sterne/Sterne.java
@@ -0,0 +1,45 @@
+package Sterne;
+
+
+public class Sterne
+{
+	public static void zeichnenQuadrat()
+	{
+		zeichnenSternzeile(7);
+		for (int i = 1; i <= 5; i++)
+		{
+		zeichnenSternrand(7);
+		}
+		zeichnenSternzeile(7);
+	}
+
+	private static void zeichnenSternzeile(int pLänge)
+	{
+		for (int i = 1; i < pLänge; i++)
+	    {
+	      System.out.print("*");
+	    }
+		 System.out.println("*");
+
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void zeichnenSternrand(int pLänge)
+	{
+		System.out.print("*");
+		for (int i = 1; i < pLänge-1; i++)
+	    {
+	      System.out.print(" ");
+	    }
+		System.out.println("*");
+		// TODO Auto-generated method stub
+
+	}
+
+
+	public static void main(String[] args)
+	   {
+		zeichnenQuadrat();
+	   }
+}
